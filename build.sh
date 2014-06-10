ca1ec6905f2208fa1020c6d6a7f08fc6bc95ab56
Yaohan Chen <yaohan.chen@gmail.com>
2014-06-10 14:12:18 -0400

    Make build.sh download javascripts if not existing

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ ! -f jquery.ba-replacetext.js ]; then
+  wget http://github.com/cowboy/jquery-replacetext/raw/master/jquery.ba-replacetext.js
+fi
+
+if [ ! -f underscore.js ]; then
+  wget http://underscorejs.org/underscore.js
+fi
+
 if [ ! -f clues.json ]; then
   phantomjs --disk-cache=true --load-images=false extract.coffee
 fi
