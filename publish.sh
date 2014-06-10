81b3b64c21975e8e5c111d5bcc3f75b9496bebd8
Yaohan Chen <yaohan.chen@gmail.com>
2014-06-09 23:25:43 -0400

    Make publish.sh push the changes

--- publish.sh
+++ publish.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+set -e
 [ ! -f clues.json ] && phantomjs --disk-cache=true --load-images=false extract.coffee
 mv clues.json clues.json.new
 git checkout gh-pages
 git checkout master -- index.html
 mv clues.json.new clues.json
 git add clues.json
-
+git commit
+git push origin gh-pages:gh-pages
