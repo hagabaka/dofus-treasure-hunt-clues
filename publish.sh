31f87e9ba2d9046bc8f5853c689b58251095fad6
Yaohan Chen <yaohan.chen@gmail.com>
2014-06-12 23:22:22 -0400

    Do not remove clues.json when publishing

--- publish.sh
+++ publish.sh
@@ -4,11 +4,12 @@ set -e
 if [ ! -f clues.json ]; then
   ./build.sh
 fi
-mv clues.json clues.json.new
+cp clues.json clues.json.new
 git checkout gh-pages
 git checkout master -- index.html
-mv clues.json.new clues.json
+cp clues.json.new clues.json
 git add clues.json
 git commit
 git push origin gh-pages:gh-pages
 git checkout master
+mv clues.json.new clues.json
