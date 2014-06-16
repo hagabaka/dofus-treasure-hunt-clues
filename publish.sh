2038375ccc1eefcaf1635c88169ca194fef9ce65
Yaohan Chen <yaohan.chen@gmail.com>
2014-06-16 19:47:00 -0400

    Publish 'pretty' branch

--- publish.sh
+++ publish.sh
@@ -6,11 +6,20 @@ if [ ! -f clues.json ]; then
 fi
 cp clues.json clues.json.new
 git checkout gh-pages
+
+git checkout pretty -- index.html
+git checkout pretty -- opensearch.xml
+git checkout pretty -- jquery.event.scroll.js
+git checkout pretty -- blur-blocked.js
+mkdir -p pretty
+mv index.html opensearch.xml jquery.event.scroll.js blur-blocked.js pretty
+cp clues.json.new pretty/clues.json
+
 git checkout master -- index.html
 git checkout master -- opensearch.xml
 cp clues.json.new clues.json
-git add clues.json
-git commit -m 'Update files'
+git commit -am 'Update files'
 git push origin gh-pages:gh-pages
+
 git checkout master
 mv clues.json.new clues.json
