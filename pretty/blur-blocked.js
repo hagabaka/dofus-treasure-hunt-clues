408515985d207adc7a35973504f9394fd5603936
Yaohan Chen <yaohan.chen@gmail.com>
2014-06-18 16:56:45 -0400

    Update files

--- pretty/blur-blocked.js
+++ pretty/blur-blocked.js
@@ -1,46 +1,113 @@
-var blurBlocked = (function() {
-  var my = {}
+// Return list of coordinates as used in CSS polygon expressions
+function polygon(points) {
+  return 'polygon(' + points.map(function(point) {
+    return point.map(function(coordinate) {
+      return coordinate + 'px';
+    }).join(' ');
+  }).join(', ') + ')';
+}
 
-  var blocking;
-  var potentiallyBlockedSelector;
-  var previouslyBlocked;
+// Rectangle class
+// coordinates is an object with properties top, right, bottom, left
+function Rectangle(coordinates) {
+  var rectangle = this;
+  ['top', 'bottom', 'left', 'right'].forEach(function(property) {
+    // assign properties top, right, bottom, left
+    rectangle[property] = coordinates[property];
+  });
+}
 
-  // _blocking is a $() wrapped element that blocks others, and
-  // _potentiallyBlockedSelector is a jQuery selector
-  my.initialize = function(_blocking, _potentiallyBlockedSelector) {
-    blocking = _blocking;
-    blocking.css('z-index', 20);
-    previouslyBlocked = $([]);
-    potentiallyBlockedSelector = _potentiallyBlockedSelector;
-  }
+['top', 'bottom'].forEach(function(y) {
+  ['left', 'right'].forEach(function(x) {
+    // define methods like topLeft(), bottomRight()
+    var capitalizedX = x.replace(/^./, function(letter) {
+      return letter.toUpperCase();
+    });
+    Rectangle.prototype[y + capitalizedX] = (function(x, y) {
+      return function() {
+        return [this[x], this[y]];
+      };
+    }(x, y));
+  });
+});
 
+// Return Rectangle with coordinates relative to given point
+Rectangle.prototype.relativeTo = function(point) {
+  var x = point[0], y = point[1];
+  return new Rectangle({
+    left:   this.left   - x,
+    right:  this.right  - x,
+    top:    this.top    - y,
+    bottom: this.bottom - y
+  });
+}
+
+// Return list of coordinates as used in CSS polygon expressions
+Rectangle.prototype.asPolygon = function() {
+  return [this.topLeft(), this.topRight(), this.bottomRight(), this.bottomLeft()];
+}
+
+// Return intersection with other rectangle
+Rectangle.prototype.intersection = function(other) {
+  return new Rectangle({
+    left   : Math.max(this.left  , other.left  ),
+    top    : Math.max(this.top   , other.top   ),
+    right  : Math.min(this.right , other.right ),
+    bottom : Math.min(this.bottom, other.bottom)
+  });
+};
+
+Rectangle.prototype.isValid = function() {
+  return this.bottom > this.top && this.right > this.left;
+};
+
+// Return Rectangle bounding the jQuery wrapped element
+Rectangle.ofElement = function(element) {
+  var elementLeft = element.offset().left;
+  var elementTop  = element.offset().top;
+  return new Rectangle({
+    left:   elementLeft,
+    top:    elementTop,
+    right:  elementLeft + element.outerWidth(),
+    bottom: elementTop  + element.outerHeight(),
+  });
+}
+
+// BlurBlocked class
+// blocking is a $() wrapped element that blocks others, and
+// potentiallyBlockedSelector is a jQuery selector
+function BlurBlocked (blocking, potentiallyBlocked) {
+  this.blocking = blocking;
+  this.potentiallyBlocked = potentiallyBlocked;
+
+  this.previouslyBlocked = $([]);
+  this.blocking.css('z-index', 20);
+}
+
+(function() {
   // Update the blur clones, should be called when page scrolls, and when potentiallyBlocked
   // elements change
-  my.update = function() {
-
-    var potentiallyBlocked = $(potentiallyBlockedSelector).not('.blur-clone');
+  BlurBlocked.prototype.update = function() {
+    var potentiallyBlocked = this.potentiallyBlocked.not('.blur-clone');
 
     // Clean up previous blur clones
-    previouslyBlocked.not(potentiallyBlocked).each(function() {
+    $('svg').detach();
+    this.previouslyBlocked.not(potentiallyBlocked).each(function() {
       removeBlurClone(this);
     });
-    previouslyBlocked = $([]);
+    this.previouslyBlocked = $([]);
 
+    var blurBlocked = this;
     potentiallyBlocked.each(function() {
       var element = $(this);
-      var elementBox = box(element);
-      var blockingBox = box(blocking);
+      var elementRectangle = Rectangle.ofElement(element);
+      var blockingRectangle = Rectangle.ofElement(blurBlocked.blocking);
 
-      blockedBox = {
-        left:   Math.max(blockingBox.left  , elementBox.left  ),
-        top:    Math.max(blockingBox.top   , elementBox.top   ),
-        right:  Math.min(blockingBox.right , elementBox.right ),
-        bottom: Math.min(blockingBox.bottom, elementBox.bottom),
-      };
+      blockedPart = elementRectangle.intersection(blockingRectangle);
 
-      if(blockedBox.bottom > blockedBox.top && blockedBox.right > blockedBox.left) {
+      if(blockedPart.isValid()) {
         // The element is blocked
-        previouslyBlocked = previouslyBlocked.add(element);
+        blurBlocked.previouslyBlocked = blurBlocked.previouslyBlocked.add(element);
 
         // Create a blurred clone if this element doe not have one
         if(!this.__blurClone__) {
@@ -52,48 +119,27 @@ var blurBlocked = (function() {
 
         // Position the blurred clone to overlap the element, and clip it to the
         // blocking element
-        var clipBox = {
-          top:    Math.max(blockingBox.top    - elementBox.top,  -1),
-          left:   Math.max(blockingBox.left   - elementBox.left, -1),
-          bottom: Math.max(blockingBox.bottom - elementBox.top ,  1),
-          right:  Math.max(blockingBox.right  - elementBox.left,  1),
-        };
-        var clipPx = boxWithPx(clipBox);
-        this.__blurClone__.offset(elementBox).css('-webkit-clip-path', boxToPolygon(clipPx));
+        var cloneClip = blockingRectangle.relativeTo(elementRectangle.topLeft());
+        this.__blurClone__.offset(elementRectangle)
+                          .clipPath(cloneClip.asPolygon(), {svgDefId: newId()});
 
-        /*      1---------------2
-         *      |               |
-         *      10-+-------9    |
-         * y -> 5--6       |    |
-         *      |  |       |    |
-         *      |  7-------8    |
-         *      4---------------3
-         *
-         *
-         *
-         */
-        var elementPx = boxWithPx({
-          left:   -1,
-          top:    -1,
-          right:  element.outerWidth()  + 1,
-          bottom: element.outerHeight() + 1,
-        });
-        var y = (clipBox.top - 1).toString() + 'px';
-        var right = (elementBox.right + 1).toString() + 'px';
-        var elementPolygon =  polygon(
-          [elementPx.left,  elementPx.top   ],
-          [elementPx.right, elementPx.top   ],
-          [elementPx.right, elementPx.bottom],
-          [elementPx.left,  elementPx.bottom],
-          [elementPx.left,  y               ],
-          [clipPx.left,     y               ],
-          [clipPx.left,     clipPx.bottom   ],
-          [clipPx.right,    clipPx.bottom   ],
-          [clipPx.right,    clipPx.top      ],
-          [elementPx.left,  clipPx.top      ]
-        );
-        console.log(elementPolygon);
-        element.css('-webkit-clip-path', elementPolygon);
+        // Clip the original element to everything except the blocking element
+        //     5
+        //    1+-----------+2
+        //     |\10        |
+        //     |6+-------+9|
+        //     | |       | |
+        //     |7+-------+8|
+        //     |           |
+        //    4+-----------+3
+        var everything = new Rectangle({top: -5000, left: -5000, bottom: 10000, right: 10000});
+        var elementClipPath = [
+              everything.topLeft(), everything.topRight(), everything.bottomRight(),
+              everything.bottomLeft(), everything.topLeft(), cloneClip.topLeft(),
+              cloneClip.bottomLeft(), cloneClip.bottomRight(), cloneClip.topRight(),
+              cloneClip.topLeft()
+            ];
+        element.clipPath(elementClipPath, {svgDefId: newId()});
       } else {
         removeBlurClone(this);
       }
@@ -101,48 +147,12 @@ var blurBlocked = (function() {
   }
 
   // Remove blur clones
-  my.clear = function() {
-    previouslyBlocked.each(function() {
+  BlurBlocked.prototype.clear = function() {
+    this.previouslyBlocked.each(function() {
       removeBlurClone(this);
     });
   }
 
-  // Return object containing the left, top, right and bottom coordinates
-  function box(element) {
-    var elementLeft = element.offset().left;
-    var elementTop  = element.offset().top;
-    return  {
-      left:   elementLeft,
-      top:    elementTop,
-      right:  elementLeft + element.outerWidth(),
-      bottom: elementTop  + element.outerHeight(),
-    };
-  }
-
-  // Append 'px' to all properties
-  function boxWithPx(regularBox) {
-    newBox = {}
-    for(coordinate in regularBox) {
-      newBox[coordinate] = regularBox[coordinate] + 'px';
-    }
-    return newBox;
-  }
-
-  // Return list of coordinates as used in CSS polygon expressions
-  function boxToPolygon(_box) {
-    return polygon([_box.left , _box.top   ],
-                   [_box.right, _box.top   ],
-                   [_box.right, _box.bottom],
-                   [_box.left , _box.bottom]);
-  }
-
-  // Return list of coordinates as used in CSS polygon expressions
-  function polygon() {
-    var points = $.makeArray(arguments);
-    return 'polygon(' + points.map(function(point) {
-      return point.join(' ');
-    }).join(', ') + ')';
-  }
   // Delete and detach the blurred clone
   function removeBlurClone(node) {
     if(node.__blurClone__) {
@@ -152,6 +162,9 @@ var blurBlocked = (function() {
     }
   }
 
-  return my;
+  var id = 0;
+  function newId() {
+    return 'blur-blocked-clip-path-' + (id++).toString();
+  }
 } ());
 
