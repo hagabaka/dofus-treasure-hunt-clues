05bd253b915b926e288e5031116b0af0102b40ab
Yaohan Chen <yaohan.chen@gmail.com>
2014-10-07 05:27:28 -0400

    Automatically download sugar-date.js

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ if [ ! -f underscore.js ]; then
   wget http://underscorejs.org/underscore.js
 fi
 
+if [ ! -f sugar-date.js ]; then
+  curl -o sugar-date.js -d core_package=core -d date_package=date sugarjs.com/download
+fi
+
 phantomjs --disk-cache=true --load-images=false extract.coffee
 mv clues.json site/
 git show gh-pages:clues.json | git diff --no-index -- - site/clues.json || true
