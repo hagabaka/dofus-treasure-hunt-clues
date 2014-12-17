a67364e0bb3a53f508783a8fbb642710e51cace5
Yaohan Chen <yaohan.chen@gmail.com>
2014-12-17 02:43:19 -0500

    Remove unnecessary building script

--- publish.sh
+++ publish.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 set -e
-if [ ! -f site/clues.json ]; then
-  ./build.sh
-fi
-cp site/clues.json clues.json.new
 git checkout gh-pages
 
 git checkout master -- site/
 git mv -fk site/* .
-cp clues.json.new clues.json
 git commit -am 'Update files'
 git push origin gh-pages:gh-pages
 
 git checkout master
-mv clues.json.new site/clues.json
